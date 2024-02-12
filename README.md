5ccabdd60b48ba4cc7e65d0498cf958af9ff31fa
coderunner86 <java@unicauca.edu.com>
2024-02-12 11:25:20 -0500

    Añadir archivo .gitignore

---
+++ README.md
@@ -0,0 +1,39 @@
+#Web Scraping with BeautifulSoup (bs4) in Python
+This repository contains a Python script for web scraping using the BeautifulSoup library. The script is designed to extract job information from a website and filter out specific job titles, such as those related to Python development.
+
+##Prerequisites
+Before running the script, make sure you have Python installed on your machine.
+
+##Usage
+Clone the repository to your local machine:
+
+`git clone https://github.com/coderunner86/python-web-scraper-tutorial.git`
+
+Navigate to the project directory:
+
+`cd python-web-scraper-tutorial`
+
+# Create a virtual environment
+
+`python -m venv venv`
+
+# Activate the virtual environment
+# On Windows:
+
+`venv\Scripts\activate`
+
+# On macOS/Linux:
+
+`source venv/bin/activate`
+
+# Install the required packages
+
+`pip install -r requirements.txt`
+
+Run the Python script:
+
+`python main.py`
+
+# Acknowledgment
+This project is based on the tutorial provided by Real Python. You can find the original tutorial here
+https://realpython.com/beautiful-soup-web-scraper-python/
---
+++ main.py
@@ -0,0 +1,54 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://realpython.github.io/fake-jobs/"
+page = requests.get(URL)
+#Uncomment this line to see what you got
+#print(page.text)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+#finding elements
+job_titles = soup.find_all("h2", class_="title is-5")
+company_names = soup.find_all("h3", class_="subtitle is-6 company")
+jobs = []
+companys = []
+
+for title, company in zip(job_titles, company_names):
+    jobs.append(title.text)
+    companys.append(company.text)
+
+# You can uncomment if you want to see the scraped content    
+# print(job_titles)
+# print(company_names)
+# print(jobs)
+# print(companys)
+
+print('Filter by class')
+res1 = dict(zip(jobs, companys))
+print('\n Dict with key job title and value company name:')
+print(f'\n {res1}')
+print('\n Filter by content')
+res2 = soup.find_all("h2", string=lambda text:"python" in text.lower())
+print('We got a (ResultSet), a set og  Beautiful Soup objects corresponding to a <h2> tags respectively')
+print(f'\n {res2}')
+
+# Convert Beautiful Soup objects to strings to create a new BeautifulSoup object
+res2_strings = [str(tag) for tag in res2]
+#create the object
+soup2 = BeautifulSoup(''.join(res2_strings), 'html.parser')
+# Get the content of the filtered <h2> elements
+h2_cont = []
+for h2_tag in soup.find_all('h2', class_='title is-5'):
+    h2_content = h2_tag.get_text(strip=True)
+    h2_cont.append(h2_content)
+print('\n Cleaning the data:')
+print(f'\n {h2_cont}')
+
+print('\n Filter by word content (e.g. python)')
+#h2_tag_python is a list of Beautiful Soup objects
+# that meet the criteria of containing the word "python"
+h2_tag_python = [tag for tag in res2 if "python" in tag.get_text(strip=True).lower()]
+#h2_cont_py is a list containing only the text of the <h2> elements
+h2_cont_py = [tag.get_text(strip=True) for tag in h2_tag_python]
+print(f'\n {h2_cont_py}')
